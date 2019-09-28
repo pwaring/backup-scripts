2bb9fa422e96f3b122de62e52fb88783af319d76
Paul Waring <paul@xk7.net>
2019-09-28 14:37:39 +0100

    Make Borg include/exclude files relative to script

--- borg/backup.sh
+++ borg/backup.sh
@@ -5,6 +5,7 @@ set -e
 set -x
 
 REPO=$1
+CURRENT_DIR=$(dirname $0)
 
 # Borg requires that archive names are unique and you have to create a new
 # archive each time, so we use the hostname and the current date/time
@@ -12,7 +13,8 @@ HOSTNAME=$(hostname)
 CURRENT_DATE=$(date +'%Y-%m-%d-%H-%M-%S')
 ARCHIVE_NAME="${HOSTNAME}-${CURRENT_DATE}"
 REPO_ARCHIVE="${REPO}::${ARCHIVE_NAME}"
-INCLUDE_DIRS=$(cat ../${HOSTNAME}-include)
+INCLUDE_DIRS=$(cat ${CURRENT_DIR}/../${HOSTNAME}-include)
+EXCLUDE_FILE="${CURRENT_DIR}/../${HOSTNAME}-exclude"
 
 if [ -z ${REPO} ]; then
   echo "No REPO specified"
@@ -30,7 +32,7 @@ if [ ! -f ${REPO}/config ]; then
 fi
 
 # Backup everything and check it
-borg create -v --progress --stats ${REPO_ARCHIVE} ${INCLUDE_DIRS} --exclude-from ../${HOSTNAME}-exclude
+borg create -v --progress --stats ${REPO_ARCHIVE} ${INCLUDE_DIRS} --exclude-from ${EXCLUDE_FILE}
 borg check --progress ${REPO}
 
 # Prune and then check again, because we're paranoid
