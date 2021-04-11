d18fab3e751c6c8aaef7902d56ee24cfcaf20ab3
Paul Waring <paul@xk7.net>
2021-04-11 13:38:59 +0100

    Update all.sh to pass shellcheck with no issues

--- all.sh
+++ all.sh
@@ -6,25 +6,25 @@ set -x
 set -o pipefail
 
 BASE_REPO=$1
-CURRENT_DIR=$(dirname $0)
+CURRENT_DIR=$(dirname "${0}")
 HOSTNAME=$(hostname)
 INCLUDE_FILE="${CURRENT_DIR}/${HOSTNAME}-include"
 EXCLUDE_FILE="${CURRENT_DIR}/${HOSTNAME}-exclude"
 
 # Check that all include and exclude directories exist
-while read line; do
-  if [ ! -d ${line} ]; then
+while read -r line; do
+  if [ ! -d "${line}" ]; then
     echo "${line} does not exist in ${INCLUDE_FILE}"
     exit 1
   fi
-done < ${INCLUDE_FILE}
+done < "${INCLUDE_FILE}"
 
-while read line; do
-  if [ ! -d ${line} ]; then
+while read -r line; do
+  if [ ! -d "${line}" ]; then
     echo "${line} does not exist in ${EXCLUDE_FILE}"
     exit 1
   fi
-done < ${EXCLUDE_FILE}
+done < "${EXCLUDE_FILE}"
 
 if [ -z "${BASE_REPO}" ]; then
   echo "No REPO specified"
@@ -40,7 +40,7 @@ BORG_REPO="${BASE_REPO}/borg/"
 RESTIC_REPO="${BASE_REPO}/restic/"
 TAR_REPO="${BASE_REPO}/tar/"
 
-if [ -e ${CURRENT_DIR}/pre-backup.sh ]; then
+if [ -e "${CURRENT_DIR}"/pre-backup.sh ]; then
   # Only run pre-backups if this has not been done for more than 4 hours, as this is an expensive operation
   # This is useful when we are running the all.sh backup script multiple times in quick succession, where
   # the pre-backup information is unlikely to have changed
@@ -61,11 +61,11 @@ if [ -e ${CURRENT_DIR}/pre-backup.sh ]; then
   fi
 
   if [ $PB_RUN -eq 1 ]; then
-    /bin/bash ${CURRENT_DIR}/pre-backup.sh
-    touch ${PB_RUN_FILE}
+    /bin/bash "${CURRENT_DIR}"/pre-backup.sh
+    touch "${PB_RUN_FILE}"
   fi
 fi
 
-/bin/bash ${CURRENT_DIR}/borg.sh "${BORG_REPO}"
-/bin/bash ${CURRENT_DIR}/restic.sh "${RESTIC_REPO}"
-/bin/bash ${CURRENT_DIR}/tar.sh "${TAR_REPO}"
+/bin/bash "${CURRENT_DIR}"/borg.sh "${BORG_REPO}"
+/bin/bash "${CURRENT_DIR}"/restic.sh "${RESTIC_REPO}"
+/bin/bash "${CURRENT_DIR}"/tar.sh "${TAR_REPO}"
